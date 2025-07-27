53923d290a47a57a7b020d8eeced54a1c20c6e65
haroldCoder <haroldc2005@gmail.com>
2025-07-26 19:19:22 -0500

    feature; change json

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const jsonServer = require("json-server");
 const morgan = require("morgan");
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("transactions.json");
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3002;
 
 server.use(middlewares);
 server.use(morgan("dev"));
-server.use((req, res, next) => { // Middleware to disable CORS
+server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 })
